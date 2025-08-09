ed1376e770c56784b999c219613961b98d3df29a
Pratham Mishra <99235987+Pratham-Mishra04@users.noreply.github.com>
2025-08-09 20:47:00 +0530

    chore: setting request type in core ctx

--- core/schemas/plugin.go
+++ core/schemas/plugin.go
@@ -6,8 +6,9 @@ import "context"
 // PluginShortCircuit represents a plugin's decision to short-circuit the normal flow.
 // It can contain either a response (success short-circuit) or an error (error short-circuit).
 type PluginShortCircuit struct {
-	Response *BifrostResponse // If set, short-circuit with this response (skips provider call)
-	Error    *BifrostError    // If set, short-circuit with this error (can set AllowFallbacks field)
+	Response *BifrostResponse    // If set, short-circuit with this response (skips provider call)
+	Stream   chan *BifrostStream // If set, short-circuit with this stream (skips provider call)
+	Error    *BifrostError       // If set, short-circuit with this error (can set AllowFallbacks field)
 }
 
 // Plugin defines the interface for Bifrost plugins.
