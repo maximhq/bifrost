48dcdf9c8233f3d5278b54194a58ff50c3e8bde8
Davshiv20 <shivamdave2903@gmail.com>
2025-12-12 18:49:08 +0530

    review fixes

--- core/providers/ollama/embedding.go
+++ core/providers/ollama/embedding.go
@@ -6,7 +6,6 @@ import (
 	"github.com/maximhq/bifrost/core/schemas"
 )
 
-// ToOllamaEmbeddingRequest converts a Bifrost embedding request to Ollama native format.
 func ToOllamaEmbeddingRequest(bifrostReq *schemas.BifrostEmbeddingRequest) *OllamaEmbeddingRequest {
 	if bifrostReq == nil {
 		return nil
@@ -71,13 +70,32 @@ func (r *OllamaEmbeddingRequest) ToBifrostEmbeddingRequest() *schemas.BifrostEmb
 	// Convert input to EmbeddingInput
 	if r.Input != nil {
 		input := &schemas.EmbeddingInput{}
+		converted := false
 		switch v := r.Input.(type) {
 		case string:
 			input.Text = &v
+			converted = true
 		case []string:
 			input.Texts = v
+			converted = true
+		case []interface{}:
+			ss := make([]string, 0, len(v))
+			for _, it := range v {
+				s, ok := it.(string)
+				if !ok {
+					converted = false
+					break
+				}
+				ss = append(ss, s)
+			}
+			if len(ss) > 0 {
+				input.Texts = ss
+				converted = true
+			}
 		}
-		bifrostReq.Input = input
+		if converted {
+			bifrostReq.Input = input
+		}
 	}
 
 	// Map Ollama-specific options back to extra params
