dd00e872c7f43f4c2a155e53e681d3ab8b383005
TejasGhatte <tejasghatte5@gmail.com>
2025-12-29 18:29:08 +0530

    feat: file input test cases for core

--- core/providers/openrouter/openrouter_test.go
+++ core/providers/openrouter/openrouter_test.go
@@ -42,6 +42,8 @@ func TestOpenRouter(t *testing.T) {
 			ImageURL:              false, // OpenRouter's responses API is in Beta
 			ImageBase64:           false, // OpenRouter's responses API is in Beta
 			MultipleImages:        false, // OpenRouter's responses API is in Beta
+			FileBase64:            true,
+			FileURL:               true,
 			CompleteEnd2End:       false, // OpenRouter's responses API is in Beta
 			Reasoning:             true,
 			ListModels:            true,
