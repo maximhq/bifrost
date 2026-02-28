c62f690e0e5f544b547887213254fa0d4f5622ce
ReStranger <restranger@disroot.org>
2026-02-28 21:52:07 +0300

    nix: extract nix devshell

--- flake.nix
+++ flake.nix
@@ -76,57 +76,10 @@
       # Or if you use direnv:
       # direnv allow
       devShells = forEachSupportedSystem (
-        {
-          pkgs,
-        }:
+        { pkgs, ... }:
         {
           # Run `nix develop` to activate this environment or `direnv allow` if you have direnv installed
-          default = pkgs.mkShellNoCC {
-            # The name of the environment
-            name = "bifrost-nix-dev-shell";
-
-            # The Nix packages provided in the environment
-            packages = with pkgs; [
-              go
-              gopls
-              gofumpt
-              air
-              delve # provides dlv
-              go-tools # provides staticcheck
-
-              nodejs_24
-            ];
-
-            # Set any environment variables for your development environment
-            env = { };
-
-            # Add any shell logic you want executed when the environment is activated
-            shellHook = ''
-              ##
-              ## Go: project-local GOPATH/GOBIN
-              ##
-              export GOPATH="$PWD/.nix-store/go"
-              export GOBIN="$GOPATH/bin"
-              export GOMODCACHE="$GOPATH/pkg/mod"
-              export GOCACHE="$PWD/.nix-store/gocache"
-
-              mkdir -p "$GOBIN" "$GOMODCACHE" "$GOCACHE"
-
-              export PATH="$PATH:$GOBIN"
-
-              ##
-              ## Node: project-local "global" npm prefix
-              ##
-              # npm reads npm_config_prefix (or NPM_CONFIG_PREFIX) as the "prefix" config,
-              # which is where `npm i -g` installs to.
-              export npm_config_prefix="$PWD/.nix-store/npm-global"
-
-              mkdir -p "$npm_config_prefix/bin"
-
-              # Ensure "global" npm bin is on PATH for this shell only
-              export PATH="$PATH:$npm_config_prefix/bin"
-            '';
-          };
+          default = import ./nix/devshells/default.nix { inherit pkgs; };
         }
       );
     };
