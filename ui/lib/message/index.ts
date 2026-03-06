9057e544f5eeed151f8368401cba1c5386fb6f77
Suresh Chaudhary <hello@suresh.im>
2026-03-06 20:21:05 +0530

    prompt repository changes
    feat/prompt-playground-ui-components
    
    WIP: prompt playground draft

---
+++ ui/lib/message/index.ts
@@ -0,0 +1,18 @@
+export { Message } from './message'
+export {
+  MessageRole,
+  MessageType,
+  type APIMessage,
+  type CompletionRequest,
+  type CompletionResult,
+  type CompletionResultChoice,
+  type MessageContent,
+  type MessageError,
+  type MessageFile,
+  type MessageImageURL,
+  type MessageInputAudio,
+  type SerializedMessage,
+  type ToolCall,
+  type ToolCallFunction,
+  type ToolResult,
+} from './types'
--- ui/lib/store/apis/baseApi.ts
+++ ui/lib/store/apis/baseApi.ts
@@ -168,6 +168,10 @@ export const baseApi = createApi({
 		"MCPToolGroups",
 		"AuditLogs",
 		"UserGovernance",
+		"Folders",
+		"Prompts",
+		"Versions",
+		"Sessions",
 	],
 	endpoints: () => ({}),
 });
