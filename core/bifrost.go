7f1c2464cf385b4fae2f4206972122ae32d33313
Samyabrata Maji <116789799+sammaji@users.noreply.github.com>
2026-03-16 19:29:18 +0530

    fix: handling text, vtt, srt response format for transcriptions

--- core/bifrost.go
+++ core/bifrost.go
@@ -5167,6 +5167,7 @@ func (bifrost *Bifrost) handleProviderRequest(provider schemas.Provider, req *Ch
 		if bifrostError != nil {
 			return nil, bifrostError
 		}
+		transcriptionResponse.BackfillParams(req.BifrostRequest.TranscriptionRequest)
 		response.TranscriptionResponse = transcriptionResponse
 	case schemas.ImageGenerationRequest:
 		imageResponse, bifrostError := provider.ImageGeneration(req.Context, key, req.BifrostRequest.ImageGenerationRequest)
