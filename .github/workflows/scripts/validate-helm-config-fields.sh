53662eedb5b9b509a91a7ac09a23252988e8e131
Anuj Parihar <anujparihar@yahoo.com>
2026-04-15 16:59:00 +0530

    chore: extend helm test case for scim

--- .github/workflows/scripts/validate-helm-config-fields.sh
+++ .github/workflows/scripts/validate-helm-config-fields.sh
@@ -896,6 +896,10 @@ assert_field_value 'scim_config.enabled' '.scim_config.enabled' 'true'
 assert_field_value 'scim_config.provider' '.scim_config.provider' '"okta"'
 assert_field 'scim_config.config' '.scim_config.config'
 assert_field 'scim_config.config.apiToken' '.scim_config.config.apiToken'
+assert_field 'scim_config.config.clientSecret' '.scim_config.config.clientSecret'
+assert_field_value 'scim_config (entra) enabled' '.scim_config.enabled' 'true'
+assert_field 'scim_config (entra) config.tenantId' '.scim_config.config.tenantId'
+assert_field 'scim_config (entra) config.clientId' '.scim_config.config.clientId'
 
 # SCIM - Entra
 cat > "$TMPDIR/values-scim-entra.yaml" << 'VALS'
